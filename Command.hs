16289b2d5fc9c0406c5276da6e9d59c3d31c1141
MalikL2005 <your_email@example.com>
2026-05-17 16:33:01 +0200

    execute commands

--- Command.hs
+++ Command.hs
@@ -1,8 +1,20 @@
 module Command where
 
+import System.Exit (exitSuccess)
+
 
 data CMD 
   = EXIT
   | HELP
   | CLEAR
   deriving (Show, Eq)
+
+executeCmd :: CMD -> IO ()
+executeCmd EXIT = do 
+  putStrLn "Good bye then."
+  exitSuccess
+executeCmd HELP = putStrLn "I am trying to help you"
+executeCmd CLEAR = clearScreen
+
+clearScreen :: IO ()
+clearScreen = putStr "\ESC[2J\ESC[1;1H"
--- Main.hs
+++ Main.hs
@@ -1,34 +1,33 @@
 module Main where
 
+import Command (CMD, executeCmd)
 import Control.Monad.State (MonadIO (liftIO), execState)
-import Data.Char (isDigit, isSpace)
 import Evaluator
-import GHC.IO.FD (stdout)
-import GHC.IO.Handle (hFlush)
 import Lexer
 import Parser
 import System.Environment
-import Test
 import System.IO
+import Test
 
 main :: IO ()
 main = do
   hSetBuffering stdin LineBuffering
+  hSetBuffering stdout NoBuffering
   args <- getArgs
   -- if args.first == "--test": runTests
-  loop "> " []
-  print "Bye."
+  loop "> " [] []
 
-loop :: String -> [String] -> IO ()
-loop prompt args = do
+loop :: String -> [String] -> [String] -> IO ()
+loop prompt args inputs = do
   putStr prompt
+  -- TODO: allow user to press arrow up/down to go to last cmds
   input <- getLine
   putStrLn $ "Lexer result: " ++ show (lexer input)
   putStrLn $ "Parser result: " ++ show (parse (lexer input))
   let result = run input
-  case result of 
-    EvalCmd cmd -> putStrLn $ "Executing command: " ++ show cmd
+  case result of
+    EvalCmd cmd -> executeCmd cmd
     EvalNum n -> putStrLn $ "Result (numeric): " ++ show n
     EvalBool b -> putStrLn $ "Result (boolean): " ++ show b
     EvalError err -> putStrLn $ "Error: " ++ err
-  if input == "exit" then return () else loop prompt args
+  loop prompt args (inputs ++ [input])
